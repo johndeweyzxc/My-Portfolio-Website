d929987b2fc550edf0c690951b80735a0b152812
johndeweyzxc <johndewey02003@gmail.com>
2023-02-20 21:52:08 +0800

    Implemented firestore database to store website traffic information

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -1,6 +1,64 @@
 import PageIndicator1 from "../assets/About/PageIndicator1.png";
+import {initializeApp} from "firebase/app";
+import {collection, getCountFromServer, getFirestore} from "firebase/firestore";
+import {addDoc} from "firebase/firestore";
+import {useEffect, useState, useRef} from "react";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  TRAFFIC_INFO_REF,
+  VISITORS_COL,
+} from "../apikey";
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export default function About({aboutRef, footerRef}) {
+  const [totalVisitor, setTotalVisitor] = useState(0);
+  const dataFetchedRef = useRef(false);
+
+  const sendIpInformation = async (countryCode, region, ip) => {
+    await addDoc(collection(db, TRAFFIC_INFO_REF + VISITORS_COL), {
+      ip: ip,
+      countryCode: countryCode,
+      region: region,
+    });
+  };
+
+  const getIpInformation = async () => {
+    fetch("http://ip-api.com/json/")
+      .then((response) => response.json())
+      .then((data) => {
+        sendIpInformation(data.countryCode, data.region, data.query);
+      });
+  };
+
+  const getTotalNumberOfVisitors = async () => {
+    const visitorCollection = collection(db, TRAFFIC_INFO_REF + VISITORS_COL);
+    const snapshot = await getCountFromServer(visitorCollection);
+    setTotalVisitor(snapshot.data().count + 1);
+  };
+
+  useEffect(() => {
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+    getIpInformation();
+    getTotalNumberOfVisitors();
+  }, []);
+
   return (
     <div
       className='px-32 flex items-center justify-between h-screen tablet:px-4'
@@ -47,6 +105,11 @@ export default function About({aboutRef, footerRef}) {
         >
           Connect With Me
         </button>
+
+        <div className='mt-16 flex font-Inconsolata font-semibold text-lg text-secondaryTextColor'>
+          <div className='mr-4'>Total number of visits:</div>
+          <div className='text-primaryTextColor'>{totalVisitor}</div>
+        </div>
       </div>
       <img src={PageIndicator1} className='laptop:hidden' />
     </div>
